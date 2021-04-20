916c39273867bb619958f672423019f965b36f72
Alexander Morales <alexandermorales.n1@gmail.com>
2021-04-19 19:04:20 -0500

    fix linter errors

--- script.rb
+++ script.rb
@@ -1,40 +1,37 @@
 # sorting array of integers
 
 def bubble_sort(arr)
-changes = true
+  changes = true
 
-while changes do
+  while changes
     changes = false
-    (arr.size-1).times do |i|
-        if arr[i] > arr[i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+    (arr.size - 1).times do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         changes = true
-         end
+      end
     end
-end
-puts arr
+  end
+  puts arr
 end
 
 # sorting array of strings
 
 def bubble_sort_by(arr)
-    changes = true
-    
-    while changes do
-        changes = false
-        (arr.size-1).times do |i|
-            if arr[i] < arr[i+1]
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            changes = true
-             end
-        end
+  changes = true
+  while changes
+    changes = false
+    (arr.size - 1).times do |i|
+      if arr[i] < arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        changes = true
+      end
     end
-    puts arr
-    end
-    
-# calling methods 
+  end
+  puts arr
+end
 
-bubble_sort([1,8,2,3,10,5,8])
-bubble_sort_by(['hey', 'hello', 'hi', 'hey'])
+# calling methods
 
-
+bubble_sort([1, 8, 2, 3, 10, 5, 8])
+bubble_sort_by(%w[hey hello hi hey])
