155cf4a1f3089e0e4c8e0c4bdfd43e53cc18d294
Alexander Morales <alexandermorales.n1@gmail.com>
2021-04-19 19:49:52 -0500

    add yield to method

--- script.rb
+++ script.rb
@@ -21,8 +21,9 @@ def bubble_sort_by(arr)
   changes = true
   while changes
     changes = false
-    (arr.size - 1).times do |i|
-      if arr[i] < arr[i + 1]
+    0.upto(arr.size - 2) do |i|
+      compare = yield arr[i], arr[i+1]
+      if compare > 0
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         changes = true
       end
@@ -34,4 +35,6 @@ end
 # calling methods
 
 bubble_sort([1, 8, 2, 3, 10, 5, 8])
-bubble_sort_by(%w[hey hello hi hey])
+bubble_sort_by(%w[hey hello hi hey]) do |str1, str2|
+  str1.length - str2.length
+end
